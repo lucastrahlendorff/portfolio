7e939dc80c0bdb150794a316f770fb531b66bc1e
Luca Strahlendorff <luca.strahlendorff@outlook.de>
2023-12-28 14:50:19 +0100

    Fallback if project not found or written

--- pages/projects/[...slug].vue
+++ pages/projects/[...slug].vue
@@ -1,11 +1,22 @@
 <template>
   <main>
-    <ContentDoc v-slot="{ doc }">
-      <article>
-        <h1 class="title">{{ doc.title }}</h1>
-        <Tags :tags="doc.tags"></Tags>
-        <ContentRenderer :value="doc" />
-      </article>
+    <ContentDoc>
+      <template v-slot="{ doc }">
+        <article>
+          <h1 class="title">{{ doc.title }}</h1>
+          <Tags :tags="doc.tags"></Tags>
+          <ContentRenderer :value="doc" />
+        </article>
+      </template>
+    <template #empty>
+      <h1>Oh no! 👀</h1>
+      This page has not been written yet. Come back later!
+    </template>
+    <template #not-found>
+      <h1>Hmmmmm... 👀</h1>
+      Sorry, but I can't find the page you are looking for.
+      Maybe you will find it under <NuxtLink to="/projects">Projects</NuxtLink> ^^
+    </template>
     </ContentDoc>
   </main>
 </template>
