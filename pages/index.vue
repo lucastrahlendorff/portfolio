fe283c2f5b121af95ba875fe80f51987e7ee0231
Luca Strahlendorff <luca.strahlendorff@outlook.de>
2024-08-30 19:16:27 +0200

    Index: Fix line height and add fallback font

--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,14 @@
     <div class="hero">
     <h1>Hi. I'm Luca. </h1>
     <p>
-        Welcome to my portfolio of projects and experiments in 
-        <b class="clipped-text text-programming">programming</b>, 
-        <b class="clipped-text text-photography">photography</b> and 
+        Welcome to my portfolio of projects and experiments in
+        <b class="clipped-text text-programming">programming</b>,
+        <b class="clipped-text text-photography">photography</b> and
         <b class="clipped-text text-sound">sound</b>.
     </p>
-    <span class="small">Hey! This site is still under development, so if anything isn't working, <a href="mailto:luca.strahlendorff@outlook.de">let me know</a>.</span>
+    <p class="small">Hey! This site is still under development, so if anything isn't working, <a href="mailto:luca.strahlendorff@outlook.de">let me know</a>.</p>
     </div>
-    
+
 </template>
 
 <style scoped>
@@ -25,7 +25,7 @@ p {
 }
 
 .clipped-text {
-    font-family: 'GothamBold';
+    font-family: 'GothamBold', serif;
     -webkit-text-fill-color: transparent;
     -webkit-background-clip: text;
     background-color: white;
